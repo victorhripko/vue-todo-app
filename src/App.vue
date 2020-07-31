a3e95ed08c0d180ec854e39c5ef2c18e7befb50c
Victor Hripko <hripko.v@soft2bet.com>
2020-07-31 17:11:03 +0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <Todo />
+    <div class="footer">
+      <p class="footer__text">
+        Victor Hripko @DevChallenges.io
+      </p>
+    </div>
   </div>
 </template>
 
@@ -19,10 +24,32 @@ export default {
   box-sizing: border-box;
 }
 
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.footer {
+  margin-top: auto;
+  padding: 10px 20px;
+  text-align: center;
+  background-image: linear-gradient(#ddd, #ddd);
+  background-size: 400px 1px;
+  background-repeat: no-repeat;
+  background-position: center top;
+
+  &__text {
+    color: #ddd;
+  }
 }
 </style>
